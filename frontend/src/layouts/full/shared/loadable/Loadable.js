1a3adcc9f3a4202b9fa8b49298d114c182ac0692
Daniel Paul Perinchery <67698207+danielpaul-123@users.noreply.github.com>
2026-05-06 23:15:26 +0530

    fixed routing

--- frontend/src/layouts/full/shared/loadable/Loadable.js
+++ frontend/src/layouts/full/shared/loadable/Loadable.js
@@ -1,10 +1,22 @@
-import React, { Suspense } from "react";
+import React, { Suspense } from 'react';
 
-const Loadable = (Component) => (props) =>
-(
-  <Suspense>
-    <Component {...props} />
-  </Suspense>
+class ChunkErrorBoundary extends React.Component {
+  componentDidCatch(error) {
+    if (/loading chunk/i.test(error.message) || /loading css chunk/i.test(error.message)) {
+      window.location.reload();
+    }
+  }
+  render() {
+    return this.props.children;
+  }
+}
+
+const Loadable = (Component) => (props) => (
+  <ChunkErrorBoundary>
+    <Suspense fallback={null}>
+      <Component {...props} />
+    </Suspense>
+  </ChunkErrorBoundary>
 );
 
 export default Loadable;
